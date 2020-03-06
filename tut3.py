8918cd34e9a3773528bd42e02023e6be10354cde
unknown <janvijeevita@gmail.com>
2020-03-06 15:54:51 +0530

    Further information

---
+++ tut3.py
@@ -0,0 +1,20 @@
+# gui is a collection of pap widgets..that means a computer program is deciding the logic and some checkboxes,
+# menu bars etc govern gui
+
+# gui uses api to display menus checkboxes buttons etc
+# An operating system cant make a basic gui until it exposes its api
+# using tkinter we can make cross platform gui s
+
+# canvas -> used to draw shapes
+# checkbuttons-> used to make checkboxes
+# frame-> acts like div tag in web dev
+# list boxes
+# menu buttons
+
+# TO PLACE THE ABOVE ONES , we have some methods like(for geometry management)
+# pack -> places blockwise , used for packing widgets
+# grid -> makes grid like that in excel which can be accessed using row and column logic
+# .place -> places grid specifically at a place
+
+# using LABEL we'll place the text
+# LABEL->Its a widget with which user doesn't interact
