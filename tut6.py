a7fd6d857bb414478a1911a2dba456a001720127
unknown <janvijeevita@gmail.com>
2020-03-06 16:02:59 +0530

    label attributes

---
+++ tut6.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+root=Tk()
+
+root.geometry("444x334")
+root.title("This_is_tut_6")
+
+# IMPORTANT LABEL OPTIONS
+# text -adds the text
+# bd - background
+# fd - foreground
+# font - sets the font
+# padx - sets x padding
+# pady - sets y pdding
+# relief -border styling -SUNKEN, RAISED, GROOVE, RIDGE
+# SUNKEN IS GOOD
+
+title_label = Label(text ='''
+Abdul Rashid Salim Salman Khan is an Indian \nfilm actor, producer, occasional playback singer and
+television personality. In a film career spanning \nalmost thirty years, Khan has received numerous awards,
+including two National Film Awards as a film \nproducer, and two Filmfare Awards for acting. He has a 
+significant following in Asia and the Indian \ndiaspora worldwide, and is cited in the media as one of the most 
+commercially successful actors of Indian \ncinema. According to the Forbes 2018 list of Top-Paid 100 Celebrity
+Entertainers in world, Khan was \nthe highest ranked Indian with 82nd rank with earnings of
+$37.7 million.''', bg ="red", fg="white", padx=13, pady=94, font="comicsansms 9 bold", borderwidth=3, relief=SUNKEN)
+
+# Important Pack options
+# anchor = nw
+# side = top, bottom, left, right
+# fill
+# padx
+# pady
+
+# title_label.pack(side=BOTTOM, anchor ="sw", fill=X)
+title_label.pack(side=LEFT, fill=Y, padx=34, pady=34)
+
+
+root.mainloop()
