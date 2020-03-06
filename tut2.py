6cddb7eeee5ef43da1b6e24f1625e7b467e7da19
unknown <janvijeevita@gmail.com>
2020-03-06 15:51:54 +0530

    Introduction to GUI

---
+++ tut2.py
@@ -0,0 +1,7 @@
+# Tkinter is shipped with python so no need to pip install.
+from tkinter import *
+
+root = Tk()           # forms base
+# main logic
+# after applying all widgets etc:
+root.mainloop()    # makes gui interactive and remembers the logic
