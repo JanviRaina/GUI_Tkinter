84eb87ad5b251ac0d6cab4ba697dee2bfa4b90ec
unknown <janvijeevita@gmail.com>
2020-03-06 18:26:32 +0530

    Adding buttons to GUI

--- tut8.py
+++ tut8.py
@@ -7,10 +7,17 @@ from tkinter import *
 root=Tk()
 
 root.geometry("233x555")
-root.title("Frames in")
-label=Label(text="this is label")
-label.pack()
-f=Frame(root,width="200",height="100")
+root.title("Raina")
+f=Frame(root,bg="green",borderwidth=6,relief=SUNKEN)
+f.pack(side=LEFT,fill="y")
 
+f2=Frame(root,bg="blue",borderwidth=3,relief=SUNKEN)
+f2.pack(side=TOP,fill="x")
+l=Label(f2,text="WELCOME all")
+l.pack()
+
+
+label=Label(f,text="Frames in GUI",font="Helvetica 16 bold", fg="red", pady=22)
+label.pack(padx=6,pady=4)
 root.mainloop()
 
